88f34e30c70a9e987d98837f906aa0b85b60c091
Mathias Soeken <mathias.soeken@epfl.ch>
2019-02-16 17:10:56 +0100

    Best fit mapping strategy.

---
+++ test/best_fit_mapping_strategy.cpp
@@ -0,0 +1,48 @@
+#include <catch.hpp>
+
+#include <cstdint>
+
+#include <caterpillar/stg_gate.hpp>
+#include <caterpillar/synthesis/lhrs.hpp>
+#include <caterpillar/synthesis/strategies/best_fit_mapping_strategy.hpp>
+#include <caterpillar/verification/circuit_to_logic_network.hpp>
+#include <kitty/static_truth_table.hpp>
+#include <mockturtle/algorithms/simulation.hpp>
+#include <mockturtle/networks/aig.hpp>
+#include <tweedledum/io/write_unicode.hpp>
+#include <tweedledum/networks/netlist.hpp>
+
+TEST_CASE( "Best-fit mapping strategy for 3-bit sorting network", "[best_fit_mapping_strategy]" )
+{
+  using namespace caterpillar;
+  using namespace caterpillar::detail;
+  using namespace mockturtle;
+  using namespace tweedledum;
+
+  aig_network sorter;
+  const auto a = sorter.create_pi();
+  const auto b = sorter.create_pi();
+  const auto c = sorter.create_pi();
+
+  const auto w1 = sorter.create_and( a, b );
+  const auto w2 = sorter.create_and( c, w1 );
+  const auto w3 = sorter.create_and( !a, !b );
+  const auto w4 = sorter.create_and( !c, !w1 );
+  const auto w5 = sorter.create_and( !w3, !w4 );
+  const auto w6 = sorter.create_or( c, !w3 );
+
+  sorter.create_po( w2 );
+  sorter.create_po( w5 );
+  sorter.create_po( w6 );
+
+  best_fit_mapping_strategy strategy( sorter );
+  uint32_t compute{0u}, uncompute{0u};
+
+  netlist<stg_gate> circ;
+  logic_network_synthesis_stats st;
+  logic_network_synthesis<netlist<stg_gate>, aig_network, best_fit_mapping_strategy<aig_network>>(circ, sorter, {}, {}, &st);
+
+  const auto sorter2 = circuit_to_logic_network<aig_network>(circ, st.i_indexes, st.o_indexes);
+  CHECK( sorter2 );
+  CHECK( simulate<kitty::static_truth_table<3>>( sorter ) == simulate<kitty::static_truth_table<3>>( *sorter2 ) );
+}
